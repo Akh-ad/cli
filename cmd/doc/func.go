008c8c9e5064d9171adafb32786802570efe4e4e
Armad <akhmadsaaev1@gmail.com>
2024-02-05 15:31:24 +0100

    func file refactored, new command to have informations,examples, parameters of a php func

--- cmd/doc/func.go
+++ cmd/doc/func.go
@@ -3,19 +3,28 @@ package doc
 import (
 	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	funcName := "strlen"
+var funcName string
 
-	docURL := fmt.Sprintf("https://www.php.net/manual/en/function.%s.php", strings.ToLower(funcName))
+var (
+	getInfoCmd = &cobra.Command{
+		Use:   "get-info",
+		Short: "Get information about a PHP function",
+		Run: func(cmd *cobra.Command, args []string) {
+			if funcName == "" {
+				fmt.Println("Please specify the function name with the flag --function")
+				os.Exit(1)
+			}
 
-	doc, err := goquery.NewDocument(docURL)
-	if err != nil {
-		log.Fatal(err)
+			functionInfo := getFunctionInfo(funcName)
+			fmt.Println(functionInfo)
+		},
 	}
 
 	description := doc.Find(".refpurpose").First().Text()
