c7edf4b9ada2aa0126e709dfeaf1f28cddedc135
Armad <akhmadsaaev1@gmail.com>
2024-02-09 16:27:44 +0100

    return all infos about asked function

--- cmd/doc/func.go
+++ cmd/doc/func.go
@@ -10,7 +10,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var funcName string
+var (
+	funcName string
+	moreInfo bool
+)
 
 var (
 	getInfoCmd = &cobra.Command{
@@ -21,7 +24,6 @@ var (
 				fmt.Println("Please specify the function name with the flag --function")
 				os.Exit(1)
 			}
-
 			functionInfo := getFunctionInfo(funcName)
 			fmt.Println(functionInfo)
 		},
@@ -43,13 +45,42 @@ func getFunctionInfo(funcName string) string {
 		content := s.Text()
 		result += content + "\n"
 	})
+
+	if !moreInfo {
+		result += getMoreFunctionInfo(doc)
+	}
+
+	return result
+}
+
+func getMoreFunctionInfo(doc *goquery.Document) string {
+
+	var result string
+	// Description
+	doc.Find("div.refsect1.description").Each(func(i int, s *goquery.Selection){
+		descriptionContent := s.Text()
+		result += descriptionContent + "\n"
+	})
+	// Parameters
+	doc.Find("div.refsect1.parameters").Each(func(i int, s *goquery.Selection){
+		paramContent := s.Text()
+		result += paramContent + "\n"
+	})
+	// Examples
+	doc.Find("div.refsect1.examples").Each(func(i int, s *goquery.Selection){
+		paramContent := s.Text()
+		result += paramContent + "\n"
+	})
+
 	return result
 }
 
 func FuncInfoCommand(rootCmd *cobra.Command) {
 	rootCmd.AddCommand(getInfoCmd)
 
+
 	getInfoCmd.Flags().StringVarP(&funcName, "name", "n", "", "function name (required)")
 	getInfoCmd.MarkFlagRequired("name")
+	getInfoCmd.Flags().BoolVarP(&moreInfo, "moreInfos", "m", false  , "more informations about the function")
 }
 
